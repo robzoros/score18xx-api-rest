85074a195be11f5b0261ef3d72d67a4989a6bff9
Zoroastro <robzoros@gmail.com>
2017-09-05 07:50:38 +0000

    Deployment

--- app.js
+++ app.js
@@ -9,14 +9,8 @@ var express     = require("express"),
     User        = require('./models/user'), // get the mongoose model
     jwt         = require('jwt-simple'),
     fs          = require('fs'),
-    https       = require('https'),
-    key         = fs.readFileSync('./config/score18xx-key.pem'),
-    cert        = fs.readFileSync('./config/score18xx-cert.pem'),
-    global      = require('./global'),
-    https_options = {
-        key: key,
-        cert: cert
-    };
+    http        = require('http'),
+    global      = require('./global');
 
 // Connection to DB	
 mongoose.connect(config.database, function(err, res) {  
@@ -212,14 +206,9 @@ app.use('/proxy', function(req, res) {
     req.pipe(request(url)).pipe(res);
 });
 
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
-/* app.listen(3000, function() {  
-    console.log("Servidor esperando peticiones en http://local-server:Port");
-}); */
-
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
-
-https.createServer(https_options, app).listen(server_port, server_ip_address, function() {  
+http.createServer(app).listen(server_port, server_ip_address, function() {  
     console.log( "Listening on " + server_ip_address + ", server_port " + server_port );
 });
