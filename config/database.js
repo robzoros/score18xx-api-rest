7648269289d7a5a28ccd9b7b92bd07da3da3af03
Roberto Méndez <robzoros@gmail.com>
2020-11-28 11:18:15 +0100

    mongo db migration

--- config/database.js
+++ config/database.js
@@ -1,10 +1,11 @@
-var databaseURL = 'mongodb://' 
+var databaseURL = 'mongodb+srv://' 
                     + process.env.DB_USER + ':'
                     + process.env.DB_PASS + '@'
-                    + process.env.DB_URL
-                    + process.env.DB_NAME
+                    + process.env.DB_URL + '/'
+                    + process.env.DB_NAME + '?retryWrites=true&w=majority'
 module.exports = {
     'secret': process.env.SECRET_PS,
     'database': databaseURL,
     'useNewUrlParser': true
 };
+
